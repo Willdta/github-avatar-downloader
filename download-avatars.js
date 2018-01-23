1b24e7e4b167f8899633c5cfbd11afea5bb54ae9
Willdta <beyondutraining@gmail.com>
2018-01-22 20:50:20 -0500

    Added comments and finished code

--- download-avatars.js
+++ download-avatars.js
@@ -2,14 +2,23 @@ var request = require('request');
 var token = require('./secrets');
 var fs = require('fs');
 
+//Passed as arguments in Node
 var repoOwner = process.argv[2];
 var repoName = process.argv[3];
 
+//Welcome Message
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-console.log(token.GITHUB_TOKEN);
-
+//Get repoOwner/Name and pass a callback to tell function what to do
 function getRepoContributors(repoOwner, repoName, cb) {
+
+	//If both parameters don't exist, stop the call
+	if (!repoOwner || !repoName) {
+		console.log('Please enter both keys!');
+		return;
+	}
+
+	//What we are working with
   var options = {
     
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -20,34 +29,52 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
+  //Request
   request(options, (err, res, body) => {
-    cb(err, body);
+    
+    //If there's an error, throw an error
+    if (err) {
+    	throw err;
+    }
+		
+		cb(err, body);
 	});
 }
 
+//Sort object to display avatar urls
 function sortAvatars(err, body) {
 	
+	//Parse string to object
 	var parsed = JSON.parse(body);
 
+	//Loop through each avatar url
 	for (item of parsed) {
+		
+		//Call function
 		downloadImageByURL(item.avatar_url, './avatars/' + item.login + ' .jpg');
 	}
 }
 
+//Download the image
 function downloadImageByURL(url, filePath) {
 	
+
 	request(url)
+		
 		.on('error', () => {
+			
 			throw err;
 		})
 	
 		.on('response', (response) => {
-		
+			
+			//Lets us know whether request was successful
 			console.log('Status Code: ', response.statusCode)
-	
 		})
-	
+		
+		//Tell it where to go
 		.pipe(fs.createWriteStream(filePath));
 }
 
+//Call function to execute
 getRepoContributors(repoOwner, repoName, sortAvatars);

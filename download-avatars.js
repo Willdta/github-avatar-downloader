1b49a972c621285fb367dbe7b75663fc872bbf9b
Willdta <beyondutraining@gmail.com>
2018-01-22 19:06:30 -0500

    parsed and looped through each avatar url

--- download-avatars.js
+++ download-avatars.js
@@ -1,10 +1,32 @@
 var request = require('request');
+var token = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+console.log(token.GITHUB_TOKEN);
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  getRepoContributors("jquery", "jquery", function(err, result) {
-  	console.log("Errors:", err);
-  	console.log("Result:", result);
-	});
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + token.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    console.log('Status Code: ', res.statusCode)
+		cb(err, body);
+  });
+}
+
+getRepoContributors('jquery', 'jquery', sortAvatars);
+
+function sortAvatars(err, body) {
+	
+	var parsed = JSON.parse(body);
+
+	for (item of parsed) {
+		console.log(item.avatar_url);
+	}
 }

5adca5a5a0e6fd206f66f5e9bab33a990723e6e5
Willdta <beyondutraining@gmail.com>
2018-01-22 20:13:51 -0500

    Combined functions to download avatars into separate folder

--- download-avatars.js
+++ download-avatars.js
@@ -8,7 +8,9 @@ console.log(token.GITHUB_TOKEN);
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
+    
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    
     headers: {
       'User-Agent': 'request',
       'Authorization': 'token ' + token.GITHUB_TOKEN
@@ -16,9 +18,8 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, (err, res, body) => {
-    console.log('Status Code: ', res.statusCode)
-		cb(err, body);
-  });
+    cb(err, body);
+	});
 }
 
 function sortAvatars(err, body) {
@@ -26,22 +27,24 @@ function sortAvatars(err, body) {
 	var parsed = JSON.parse(body);
 
 	for (item of parsed) {
-		console.log(item.avatar_url);
+		downloadImageByURL(item.avatar_url, './avatars/' + item.login + ' .jpg');
 	}
 }
 
 function downloadImageByURL(url, filePath) {
 	
-	request.get(url, filePath)
-	.on('error', () => {
-		throw err;
-	})
-	.on('response', (response) => {
-		console.log('Status Code: ', response.statusCode)
-	})
-	.pipe(fs.createWriteStream('./avatars.jpg'));
+	request(url)
+		.on('error', () => {
+			throw err;
+		})
+	
+		.on('response', (response) => {
+		
+			console.log('Status Code: ', response.statusCode)
+	
+		})
+	
+		.pipe(fs.createWriteStream(filePath));
 }
 
 getRepoContributors('jquery', 'jquery', sortAvatars);
-
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
